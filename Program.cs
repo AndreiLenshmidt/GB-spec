b5ba4df13c2bf331c78a947db94c7f60f32c4b33
LenshmidtAndrei <lenshmidt.andrei@gmail.com>
2022-12-07 14:25:39 +0300

    Change Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateRandomArray(int min=1, int max=10)
+{
+    int arraySize = new Random().Next(min,max);
+    string[] randomArray = new string [arraySize];
+
+    for(int i = 0; i < randomArray.Length; i++)
+    {
+        int stringRandomLength = new Random().Next(min,max);
+        for(int j = 0; j < stringRandomLength; j++)
+        {
+            randomArray[i] = randomArray[i] + Convert.ToChar(new Random().Next(33, 126));
+        }
+    }
+
+    return randomArray;
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+"   ");
+    }
+    Console.WriteLine();
+}
+    
+string[] LessThreeChar(string[] array)
+{
+    int size = 0;
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length < 3) size++;
+    }
+
+    string[] newArray = new string [size];
+    
+    for(int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length < 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+
+    return newArray;
+}
+
+string[] randomArray = CreateRandomArray();
+PrintArray(randomArray);
+Console.WriteLine();
+
+string[] newArray = LessThreeChar(randomArray);
+PrintArray(newArray);
